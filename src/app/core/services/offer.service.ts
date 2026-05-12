df9dcf20c2f1d87f63f6ed9a8f5a52b12b5a79ec
Sergey Malyuga <sergey@malyuga.ru>
2026-05-12 12:33:21 +0300

    feat(offer): создать сервис

---
+++ src/app/core/services/offer.service.ts
@@ -0,0 +1,25 @@
+import {inject, Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Offer, OfferPreview} from '../models/offers';
+import {HttpClient} from '@angular/common/http';
+import {APIRoute, BASE_URL} from '../constants/const';
+import {defaultHttpPipes} from '../utils/rjxs-operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OfferService {
+  private http = inject(HttpClient);
+
+  public getOffers(): Observable<OfferPreview[]> {
+    return this.http.get<OfferPreview[]>(`${BASE_URL}/${APIRoute.OFFERS}`).pipe(...defaultHttpPipes<OfferPreview[]>());
+  }
+
+  public getOfferById(offerId: string): Observable<Offer> {
+    return this.http.get<Offer>(`${BASE_URL}/${APIRoute.OFFERS}/${offerId}`).pipe(...defaultHttpPipes<Offer>());
+  }
+
+  public getNearbyOffer(offerId: string): Observable<OfferPreview[]> {
+    return this.http.get<OfferPreview[]>(`${BASE_URL}/${APIRoute.OFFERS}/${offerId}/nearby`).pipe(...defaultHttpPipes<OfferPreview[]>());
+  }
+}
