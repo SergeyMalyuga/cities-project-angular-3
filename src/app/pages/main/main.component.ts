4e013443386d556efbd2f04f92e3c7c02a7b01f1
Sergey Malyuga <sergey@malyuga.ru>
2026-05-12 12:36:06 +0300

    feat(home-page): добавить отображение карточек офферов

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,14 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {selectOffersByCity} from '../../store/app/selectors/app.selectors';
+import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
 
 @Component({
   selector: 'app-main',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    OfferCardComponent
   ],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainComponent {
-
+  private store = inject(Store<AppState>);
+  public offers = this.store.selectSignal(selectOffersByCity);
 }
