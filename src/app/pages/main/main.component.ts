3e85ad0844d1b615e633df84325a58fabe9e96b6
Sergey Malyuga <sergey@malyuga.ru>
2026-05-13 12:23:14 +0300

    feat(main): добавить методы изменения города и типа сортировки

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,20 +1,41 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
-import {selectOffersByCity} from '../../store/app/selectors/app.selectors';
+import {selectCity, selectOffersByCity} from '../../store/app/selectors/app.selectors';
 import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
+import {CITY_LOCATIONS, SortType} from '../../core/constants/const';
+import {NgClass} from '@angular/common';
+import {City} from '../../core/models/city';
+import {changeCity} from '../../store/city/actions/city.actions';
+import {PlacesSortingFormComponent} from '../../components/places-sorting-form/places-sorting-form.component';
+import {SortByPipe} from './pipes/sort-by.pipe';
 
 @Component({
   selector: 'app-main',
   imports: [
     HeaderComponent,
-    OfferCardComponent
+    OfferCardComponent,
+    NgClass,
+    PlacesSortingFormComponent,
+    SortByPipe
   ],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainComponent {
+  protected readonly CITY_LOCATIONS = CITY_LOCATIONS;
   private store = inject(Store<AppState>);
+
   public offers = this.store.selectSignal(selectOffersByCity);
+  public currentCity = this.store.selectSignal(selectCity);
+  public sortType = signal<SortType>(SortType.POPULAR);
+
+  public changeCity(city: City): void {
+    this.store.dispatch(changeCity({city}));
+  }
+
+  public changeSortType(sortType: SortType): void {
+    this.sortType.set(sortType);
+  }
 }
