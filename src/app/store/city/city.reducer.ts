4fcd96f766ac92b153ab212046d9773b4a0f864c
Sergey Malyuga <sergey@malyuga.ru>
2026-05-13 11:58:32 +0300

    feat(city): добавить обработку выбора города

--- src/app/store/city/city.reducer.ts
+++ src/app/store/city/city.reducer.ts
@@ -1,9 +1,13 @@
 import {City} from '../../core/models/city';
 import {DEFAULT_CITY} from '../../core/constants/const';
-import {createReducer} from '@ngrx/store';
+import {createReducer, on} from '@ngrx/store';
+import {changeCity} from './actions/city.actions';
 
 const initialState: City = DEFAULT_CITY;
 
 export const cityReducer = createReducer(
   initialState,
+  on(changeCity, (state, {city}) => ({
+    ...state, ...city
+  }))
 )
