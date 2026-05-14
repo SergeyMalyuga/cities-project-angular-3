9f5b86c1de0e19172605a13d60d0ffa8ac9813d4
Sergey Malyuga <sergey@malyuga.ru>
2026-05-14 14:26:55 +0300

    feat(offer-card): добавить навигацию на страницу оффера

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -2,12 +2,15 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
 import {OfferPreview} from '../../../core/models/offers';
 import {TitleCasePipe} from '@angular/common';
 import {HoverTrackerDirective} from '../../directives/hover-tracker';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../../core/constants/const';
 
 @Component({
   selector: 'app-offer-card',
   imports: [
     TitleCasePipe,
-    HoverTrackerDirective
+    HoverTrackerDirective,
+    RouterLink
   ],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -21,4 +24,6 @@ export class OfferCardComponent {
   public onHovered(): void {
     this.hovered.emit(this.offer);
   }
+
+  protected readonly AppRoute = AppRoute;
 }
