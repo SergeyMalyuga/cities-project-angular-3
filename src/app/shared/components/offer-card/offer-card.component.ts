3d181ca103bb565a9cf39d312a78b35749a4ee86
Sergey Malyuga <sergey@malyuga.ru>
2026-05-14 10:49:04 +0300

    feat(offer-card): реализовать выбор активной карточки

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,16 +1,24 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {OfferPreview} from '../../../core/models/offers';
 import {TitleCasePipe} from '@angular/common';
+import {HoverTrackerDirective} from '../../directives/hover-tracker';
 
 @Component({
   selector: 'app-offer-card',
   imports: [
-    TitleCasePipe
+    TitleCasePipe,
+    HoverTrackerDirective
   ],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OfferCardComponent {
   @Input({required: true}) offer!: OfferPreview;
+  @Output() hovered = new EventEmitter<OfferPreview>();
+
   protected readonly Math = Math;
+
+  public onHovered(): void {
+    this.hovered.emit(this.offer);
+  }
 }
