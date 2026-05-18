d2373f7ba2336853cb0658f22034d79686974ac2
Sergey Malyuga <sergey@malyuga.ru>
2026-05-18 14:37:46 +0300

    feat(comments): ограничить форму по авторизации

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,13 +1,19 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {Comment} from '../../core/models/comments';
 import {DatePipe} from '@angular/common';
 import {CommentFormComponent} from '../comment-form/comment-form.component';
+import {SortCommentsByDatePipe} from '../../shared/directives/sort-comments-by-date.pipe';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
+import {selectAuthStatus} from '../../store/user/selectors/user.selectors';
+import {AuthorizationStatus} from '../../core/constants/const';
 
 @Component({
   selector: 'app-comments',
   imports: [
     DatePipe,
-    CommentFormComponent
+    CommentFormComponent,
+    SortCommentsByDatePipe
   ],
   templateUrl: './comments.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -16,9 +22,16 @@ export class CommentsComponent {
   @Output() postedComment = new EventEmitter<void>();
   @Input({required: true}) offerId!: string | null;
   @Input({required: true}) comments!: Comment[];
+
+  private store = inject(Store<AppState>);
   protected readonly Math = Math;
+
+  public authStatus = this.store.selectSignal(selectAuthStatus);
+
 
   public onPostedComment() {
     this.postedComment.emit();
   }
+
+  protected readonly AuthorizationStatus = AuthorizationStatus;
 }
