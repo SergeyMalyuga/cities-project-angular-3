d7a9e2013395e8e921da358e14d5e0ad55db6ed1
Sergey Malyuga <sergey@malyuga.ru>
2026-05-18 12:04:46 +0300

    feat(app): добавить в глобальный store состояния пользователя

--- src/app/store/app/app.reducer.ts
+++ src/app/store/app/app.reducer.ts
@@ -2,8 +2,10 @@ import {ActionReducerMap} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
 import {offerReducer} from '../offer/offer.reducer';
 import {cityReducer} from '../city/city.reducer';
+import {userReducer} from '../user/user.reducer';
 
 export const appReducer: ActionReducerMap<AppState> = {
   offers: offerReducer,
   city: cityReducer,
+  user: userReducer,
 }
