d082c1479a521e3c645857e438bec64ce0668116
Sergey Malyuga <sergey@malyuga.ru>
2026-05-19 15:53:49 +0300

    feat(offer-card): реализовать прокрутку страницы при клике

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -4,19 +4,22 @@ import {TitleCasePipe} from '@angular/common';
 import {HoverTrackerDirective} from '../../directives/hover-tracker';
 import {RouterLink} from '@angular/router';
 import {AppRoute} from '../../../core/constants/const';
+import {ScrollUpDirective} from '../../directives/scroll-up.directive';
 
 @Component({
   selector: 'app-offer-card',
   imports: [
     TitleCasePipe,
     HoverTrackerDirective,
-    RouterLink
+    RouterLink,
+    ScrollUpDirective
   ],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OfferCardComponent {
   @Input({required: true}) offer!: OfferPreview;
+  @Input() isOfferPage = false;
   @Output() hovered = new EventEmitter<OfferPreview>();
 
   protected readonly Math = Math;
