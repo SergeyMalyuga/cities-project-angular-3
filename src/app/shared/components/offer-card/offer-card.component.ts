4222d5224e7d00e75940d0348f088fdbc40de1f5
Sergey Malyuga <sergey@malyuga.ru>
2026-05-20 13:08:16 +0300

    feat(offer-card): добавить возможность изменения статуса избранного

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,10 +1,15 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {OfferPreview} from '../../../core/models/offers';
-import {TitleCasePipe} from '@angular/common';
+import {NgClass, TitleCasePipe} from '@angular/common';
 import {HoverTrackerDirective} from '../../directives/hover-tracker';
 import {RouterLink} from '@angular/router';
-import {AppRoute} from '../../../core/constants/const';
+import {AppRoute, AuthorizationStatus} from '../../../core/constants/const';
 import {ScrollUpDirective} from '../../directives/scroll-up.directive';
+import {FavoriteOfferService} from '../../../core/services/favorite-offer.service';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {selectAuthStatus} from '../../../store/user/selectors/user.selectors';
+import {selectIsFavoriteOffersLoading} from '../../../store/favorite-offer/selectors/favorite-offer.selectors';
 
 @Component({
   selector: 'app-offer-card',
@@ -12,7 +17,8 @@ import {ScrollUpDirective} from '../../directives/scroll-up.directive';
     TitleCasePipe,
     HoverTrackerDirective,
     RouterLink,
-    ScrollUpDirective
+    ScrollUpDirective,
+    NgClass
   ],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -22,11 +28,22 @@ export class OfferCardComponent {
   @Input() isOfferPage = false;
   @Output() hovered = new EventEmitter<OfferPreview>();
 
+  protected readonly AppRoute = AppRoute;
   protected readonly Math = Math;
+
+  private favoriteOfferService = inject(FavoriteOfferService);
+  private store = inject(Store<AppState>);
+
+  public authStatus = this.store.selectSignal(selectAuthStatus);
+  public isFavoriteOffersLoading = this.store.selectSignal(selectIsFavoriteOffersLoading);
 
   public onHovered(): void {
     this.hovered.emit(this.offer);
   }
 
-  protected readonly AppRoute = AppRoute;
+  public changeFavoriteStatus() {
+    this.favoriteOfferService.toggleFavoriteStatus(this.offer.id, !this.offer.isFavorite);
+  }
+
+  protected readonly AuthorizationStatus = AuthorizationStatus;
 }
